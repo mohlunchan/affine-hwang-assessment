b00d7d74ef182d3c56de3d75b833fae272f843b1
mohlun.chan <mohlun.chan@actechmy.com>
2021-10-09 21:48:11 +0800

    add loader, query customer by page & size

--- src/views/customer/list.vue
+++ src/views/customer/list.vue
@@ -1,18 +1,19 @@
 <template>
-  <!-- vue 3 now support fragmenting so we do not need a default root node -->
   <button @click="$router.push('/add')">Add New Customer</button>
+
   <n-data-table
+    v-model:page="page"
     :columns="columns"
-    :data="list"
+    :data="items"
     :pagination="pagination"
     :row-props="rowProps"
+    :loading="isLoading"
   />
 </template>
 
 <script>
 import { getCustomers, deleteCustomer } from "@/api/module/customers";
-import { NDataTable } from "naive-ui";
-import { debounce } from "@/libs/debounce";
+import { NDataTable, useLoadingBar } from "naive-ui";
 
 export default {
   components: {
@@ -20,9 +21,12 @@ export default {
   },
   data() {
     return {
-      list: [],
+      items: [],
+      totalItems: 0,
       pagination: {
         pageSize: 10,
+        pageCount: 10,
+        itemCount: 10,
       },
       columns: [
         {
@@ -38,15 +42,38 @@ export default {
           key: "address",
         },
       ],
+      loadingBar: useLoadingBar(),
+      isLoading: false,
     };
+  },
+  computed: {
+    page: {
+      get() {
+        return this.$route.query.page || 1;
+      },
+      set(value) {
+        this.$router.push({
+          ...this.$route,
+          query: {
+            ...this.$route.query,
+            page: value,
+          },
+        });
+
+        this.pagination.page = value;
+      },
+    },
+    pageCount() {
+      return Math.ceil(this.totalItems / this.pagination.pageSize);
+    },
   },
   methods: {
     async handleDelete(customerId) {
       try {
         await deleteCustomer(customerId);
 
-        this.list.splice(
-          this.list.findIndex((customer) => customer.id === customerId),
+        this.items.splice(
+          this.items.findIndex((customer) => customer.id === customerId),
           1
         );
       } catch {
@@ -56,21 +83,38 @@ export default {
     rowProps(row) {
       return {
         style: "cursor: pointer",
-        onClick: debounce(() => {
-          this.$router.push({
-            name: "customer-details",
-            params: {
-              id: row.id,
-            },
-          });
-        }, 300),
+        onClick: () => {
+          if (this.isLoading) return;
+
+          //prevent user from spam clicking the row
+          this.isLoading = true;
+          this.loadingBar.start();
+
+          this.$router
+            .push({
+              name: "customer-details",
+              params: {
+                id: row.id,
+              },
+            })
+            .catch(() => {
+              this.isLoading = false;
+              this.loadingBar.error();
+            });
+        },
       };
     },
   },
   beforeRouteEnter(to, from, next) {
-    getCustomers().then((response) => {
+    getCustomers(to.query.page || 1, 10).then((response) => {
       next((vm) => {
-        vm.list = response.data.map((item) => ({
+        const { total, data } = response.data;
+
+        vm.totalItems = total;
+        vm.pagination.itemCount = data.length;
+        vm.pagination.pageCount = vm.pageCount;
+
+        vm.items = data.map((item) => ({
           id: item.uuid,
           name: item.name,
           address: item.address,
@@ -78,6 +122,10 @@ export default {
         }));
       });
     });
+  },
+  beforeUnmount() {
+    this.isLoading = false;
+    this.loadingBar.finish();
   },
 };
 </script>
