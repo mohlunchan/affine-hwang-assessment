97c894e2cea086c0ea303f996c1a456af17e7c19
mohlun.chan <mohlun.chan@actechmy.com>
2021-10-10 11:47:37 +0800

    customer module route adjustments

--- src/router/routes.js
+++ src/router/routes.js
@@ -1,18 +1,28 @@
 const routes = [
     {
-        path: '/',
+        path: '', redirect: '/customers',
+    },
+    {
+        path: '/customers',
         name: 'customer-list',
         component: () => import('@/views/customer/list.vue')
     },
     {
-        path: '/:id',
-        name: 'customer-details',
-        component: () => import('@/views/customer/details.vue')
-    },
-    {
-        path: '/add',
-        name: 'customer-add',
-        component: () => import('@/views/customer/details.vue')
+        path: '/customer',
+        name: 'customer',
+        component: () => import('@/views/customer/details/index.vue'),
+        children: [
+            {
+                path: ':id',
+                name: 'customer-details',
+                component: () => import('@/views/customer/details/update.vue')
+            },
+            {
+                path: 'create',
+                name: 'customer-create',
+                component: () => import('@/views/customer/details/create.vue')
+            },
+        ]
     },
 ]
 
