eaaefca41d964175329f6749ac575afec6405fe8
mohlun.chan <mohlun.chan@actechmy.com>
2021-10-10 11:46:30 +0800

    add message provider

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,19 @@
 <template>
   <n-loading-bar-provider>
-    <router-view></router-view>
+    <n-message-provider>
+      <router-view></router-view>
+    </n-message-provider>
   </n-loading-bar-provider>
 </template>
 
 <script>
-import { NLoadingBarProvider } from "naive-ui";
+import { NLoadingBarProvider, NMessageProvider } from "naive-ui";
 
 export default {
   name: "App",
   components: {
     NLoadingBarProvider,
+    NMessageProvider,
   },
 };
 </script>
